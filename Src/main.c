0ad2dca7e0c90e956737f620076962f2e2a7144a
richa-5ingh <richa1896@gmail.com>
2025-05-17 14:33:18 +0530

    LED toggle done

--- Src/main.c
+++ Src/main.c
@@ -15,13 +15,46 @@
 // AHB1 Enable Register offset
 
 #define RCC_AHB1EN_R_OFFSET    (0x30u)
-#define RCC_AHB1EN_R           (RCC_BASE + AHB1EN_R_OFFSET)
+#define RCC_AHB1EN_R           (*(volatile unsigned int *)(RCC_BASE + RCC_AHB1EN_R_OFFSET))
 
 //GPIO MODE REGISTER
 #define GPIOA_MODE_R_OFFSET    (0x00000000UL)
-#define GPIOA_MODE_R           (GPIOA_BASE + GPIOA_MODE_R_OFFSET)
+#define GPIOA_MODE_R            (*(volatile unsigned int *)(GPIOA_BASE + GPIOA_MODE_R_OFFSET))
+
+// Output Data Register offset of GPIO
+#define OD_R_OFFSET            (0x14UL)
+// Output Data Register of GPIOA
+#define GPIOA_OD_R             (*(volatile unsigned int *)(GPIOA_BASE + OD_R_OFFSET))
 
 // GPIOAEN
 #define GPIOAEN                (1U<<0) // enabling GPIOA by giving RCC clock to bit 0
 
+#define PIN5                   (1U << 5)
+#define LED_PIN                 PIN5
 
+//Set Bit number 10
+// (1U << 10)
+// reset bit number 11
+// &= ~(1U <<11)
+
+
+
+int main (void)
+{
+	//Test : toggle LED
+
+	// 1. enable clock access to GPIO A
+	RCC_AHB1EN_R |=  GPIOAEN;
+
+	// 2. Set GPIO PIN 5 to Output
+	GPIOA_MODE_R |= (1U <<10);
+	GPIOA_MODE_R &= ~(1U <<11);
+
+	while (1)
+	{
+		//3. Toggle LED
+		GPIOA_OD_R ^= (LED_PIN);
+		for(int i =0; i< 100000; i++){}
+	}
+
+}
